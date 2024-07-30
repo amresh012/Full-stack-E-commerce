dacc813b7d3ffce61c792b4212e078c5deedfa36
Amresh-Mishra <amresgmishra67099@gmail.com>
2024-07-30 18:15:41 +0530

    new commit

--- Client/src/pages/Product/ProductPage.jsx
+++ Client/src/pages/Product/ProductPage.jsx
@@ -5,8 +5,12 @@ const Product = () => {
   return (
     <div>
       {
-        [...Array(10)].map((item)=>(
-           <Skeleton variant="rectangular" width={210} height={118} />
+        [...Array(10)].map((item) => (
+          <div className="">
+            <Skeleton variant="circular" width={40} height={40} />
+            <Skeleton variant="rectangular" width={210} height={60} />
+            <Skeleton variant="rounded" width={210} height={60} />
+          </div>
 
         ))
       }
