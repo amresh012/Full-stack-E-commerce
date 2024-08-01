5bcd4d75ea4b2f669b208eedaf4ff452c6eeafc3
Amresh-Mishra <amresgmishra67099@gmail.com>
2024-08-01 18:18:52 +0530

    code push

---
+++ Client/src/components/reusablesUI/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Loader.css"
+const Loader = () => {
+  return (
+    <div class="loader">
+  <div class="loader-text">Loading...</div>
+  <div class="loader-bar"></div>
+</div>
+
+  )
+}
+
+export default Loader
--- Client/src/pages/Product/ProductPage.jsx
+++ Client/src/pages/Product/ProductPage.jsx
@@ -1,28 +1,32 @@
 import { Skeleton } from '@mui/material'
 import React, { useState } from 'react'
 import Bestsellers from '../Bestsellers'
+import Loader from '../../components/reusablesUI/Loader'
 
 const Product = () => {
   
-  const [data , setData] = useState(true)
+  const [data , setData] = useState(false)
   
-
+   const handleProductView = ()=>{
+    setData(!data)
+   }
 
   return (
-    <div className='flex items-start justify-start '>
+    <div className='flex items-start justify-start h-screen'>
       <div className="flex flex-col justify-start gap-2 items-start pt-12 ">
       <div className="border-l-8 border-blue-500  ml-28 px-2">
-  <h1 className='text-[3rem] font-bold'>Explore Our Range Of Products</h1>
+  <h1 className='text-[3rem] font-bold' onClick={handleProductView}>Explore Our Range Of Products</h1>
   </div>
-      <div className="flex  justify-evenly w-full items-start gap-5 flex-wrap m overflow-y-scroll">
+      <div className="flex  items-center justify-center ">
       { data ?
       <Bestsellers/>
        :
-        <div className="flex flex-col flex-wrap items-center justify-around gap-2 border-2 p-2 shadow-md hover:scale-110 duration-300">
-            <Skeleton variant="rectangular" width={310} height={160} />
-            <Skeleton variant="rounded" width={310} height={100} />
-            <Skeleton variant="rounded" width={190} height={40} />
-          </div>
+       <Loader/>
+        // <div className="flex flex-col flex-wrap items-center justify-around gap-2 border-2 p-2 shadow-md hover:scale-110 duration-300">
+        //     <Skeleton variant="rectangular" width={310} height={160} />
+        //     <Skeleton variant="rounded" width={310} height={100} />
+        //     <Skeleton variant="rounded" width={190} height={40} />
+        //   </div>
       }
       </div>
       </div>
