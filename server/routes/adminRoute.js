4b5bdb711fe7e6b43335fcafc62ffb73c5613257
Amresh Mishra <73485115+amresh012@users.noreply.github.com>
2025-02-10 17:14:44 +0530

    Delete server directory

--- server/routes/adminRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAdminData } = require("../controller/adminCtrl");
-const { authMiddleware, isAdmin, checkAccess } = require("../middlewares/authMiddleware");
-const router = express.Router();
-
-router.get("/", authMiddleware,checkAccess, getAdminData);
-
-module.exports = router;
